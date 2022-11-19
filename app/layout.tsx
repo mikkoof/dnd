58909533c580519a68cadecb4143b895248d4bbb
Mikko Fredrikson <mikkoof.mf@gmail.com>
2022-11-19 22:17:00 +0200

    Simple nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from '../components/ui/navbar/navbar'
 import './globals.css'
 
 export default function RootLayout({
@@ -8,7 +9,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
