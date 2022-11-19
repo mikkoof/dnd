7135c48e8a68f41f4c5802ccdb71b43edf48594f
Mikko Fredrikson <mikkoof.mf@gmail.com>
2022-11-19 16:44:18 +0200

    Empty repo for next 13

--- lib/api/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosRequestConfig } from "axios";
-
-const config: AxiosRequestConfig = {};
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-config.baseURL = baseURL;
-
-export const axiosInstance = axios.create(config);
