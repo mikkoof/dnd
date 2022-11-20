c7783ce81e340175dbfd7d25a5cd630d40a7cbf2
Mikko Fredrikson <mikkoof.mf@gmail.com>
2022-11-20 16:24:40 +0200

    Base grid for characterSheet

---
+++ app/Sheet/page.tsx
@@ -0,0 +1,15 @@
+import styles from './sheet.module.css'
+
+export default function Sheet({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className={styles.sheet}>
+      <div>
+
+      </div>
+    </div>
+  )
+}
